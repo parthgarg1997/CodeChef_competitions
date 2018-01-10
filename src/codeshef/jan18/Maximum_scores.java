8c2a20e34a446a23211784e87579b9694481f63c
Parth Garg <parthgarg1997@gmail.com>
2018-01-11 02:01:48 +0530

    update

--- src/codeshef/jan18/Maximum_scores.java
+++ src/codeshef/jan18/Maximum_scores.java
@@ -10,7 +10,7 @@ public static void main(String args[])
 	for(int i=0;i<T;i++)
 	{
 		int sum=0;
-		int q=-1,maxp=-1;
+		int q=-1,maxp=1000000001;
 		int N=scan.nextInt();
 		int x[][]=new int[N][N];
 		for(int l=0;l<N;l++)
@@ -26,24 +26,30 @@ public static void main(String args[])
 			
 			for(int m=0;m<N;m++)
 			{
-				if(x[l][m]>max&&max<maxp)
+				//System.out.println(max+" "+maxp);
+				if((x[l][m]>max)&&(x[l][m]<maxp))
 					max=x[l][m];
+				//System.out.println(max+" "+maxp);
+
 			}
-			System.out.println(max+" "+maxp);
+			//System.out.println(max+" "+maxp);
 			if(max!=0)
 				{
 				maxp=max;
-				sum+=max;
+				sum=(sum+max)%2147483647;
 				}
 			else
 			{
-				System.out.println(-1);
+				//System.out.println(-1);
 				q=0;
 				break;
 			}
+			//System.out.println(max+" "+maxp);
 		}
 		if(q==-1)
 		System.out.println(sum);
+		else
+			System.out.println(-1);
 	}
 }
 }
